ff827f4f6992cd1064b5722b7506f55286607bf5
gram <gram@192.168.10.6>
2023-07-14 17:48:42 +0900

    20230714 컬렉션

--- JavaBasic/src/java09_api/Baskin.java
+++ JavaBasic/src/java09_api/Baskin.java
@@ -8,22 +8,18 @@ public class Baskin {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Random ran = new Random();
+//		int user = sc.nextInt(); //유저가 입력하는 숫자
+		int com; //랜덤 숫자
+		int num = 1;
 		
-		int com;
+		com = ran.nextInt(3) + 1;
+		System.out.println(com);
+		for(int i=0; i<com; i++) {
+			System.out.println(num++);
 		
-		for(int i=0; i<31; i++) {
-			System.out.print("숫자 입력 : ");
-			i = sc.nextInt();
-			
-			for(int j=i+1; j<31; j++) {
-				System.out.print("컴퓨터 차례 숫자 입력 : ");
-				System.out.println(  );
-				break;
-			}
-			
-			
 		}
 		
+		System.out.println();
 		
 	}
 }
